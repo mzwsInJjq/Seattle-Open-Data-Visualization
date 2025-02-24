7a99c4cfcb8fb053e0079c9c1cb988f5fe318a32
J. Chen <140699748+mzwsInJjq@users.noreply.github.com>
2025-02-24 15:50:05 +0800

    Remove streets outside of the city

--- seattle-snd.py
+++ seattle-snd.py
@@ -4,16 +4,21 @@
 # Seattle Street Network Database SND
 # https://data-seattlecitygis.opendata.arcgis.com/datasets/street-network-database-snd-1
 
+# Seattle GeoJSON
+# https://polygons.openstreetmap.fr/get_geojson.py?id=237385&params=0
+
 import pandas as pd
 import geopandas as gpd
 import matplotlib.pyplot as plt
 
 nman = gpd.read_file(r"C:\Users\Chen\Downloads\Neighborhood_Map_Atlas_Neighborhoods.geojson")
 snd = gpd.read_file(r"C:\Users\Chen\Downloads\Street_Network_Database_SND_5675931800012836898.geojson")
-print(snd.columns)
+seattle_geojson = gpd.read_file(r"C:\Users\Chen\Downloads\seattle.geojson")
 snd = snd[snd["CITYCODE"] == 1]
+snd = gpd.overlay(snd, seattle_geojson, how='intersection')
 
 fig, ax = plt.subplots(figsize=(20, 10))
+ax.axis('off')
 nman.plot(ax=ax, color='b', label='NMAN', alpha=0.5)
 snd.plot(ax=ax, color='k', label='SND', linewidth=0.4)
 
